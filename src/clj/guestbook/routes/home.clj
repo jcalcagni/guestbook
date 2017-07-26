5b39411d66dc2fc05ca81816aa3d79a7f257f547
jcalcagni <jcalcagni@deloitte.com>
2017-07-26 17:47:09 -0400

    added

---
+++ src/clj/guestbook/routes/home.clj
@@ -0,0 +1,19 @@
+(ns guestbook.routes.home
+  (:require [guestbook.layout :as layout]
+            [guestbook.db.core :as db]
+			[bouncer.core :as b]
+            [bouncer.validators :as v]
+            [compojure.core :refer [defroutes GET POST]]
+            [ring.util.response :refer [response status]]
+            ))
+
+(defn home-page []
+  (layout/render "home.html"))
+	
+(defn about-page []
+  (layout/render "about.html"))
+
+(defroutes home-routes
+  (GET "/" [] (home-page))
+  (GET "/messages" [] (response (db/get-messages)))
+  (GET "/about" [] (about-page)))
